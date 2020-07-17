38894d0e7b2308295755603abf993e73364518cc
faultyagatha <valeria.barvinska@gmail.com>
2020-07-17 12:05:56 +0200

    add website

---
+++ precache-manifest.a2a4dcb5ec3bdc65b735e1b07b1b434c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8936988bb38a9f2dc056537996753855",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d27ac0a5879ad63c0d64",
+    "url": "/static/css/main.a2095fbc.chunk.css"
+  },
+  {
+    "revision": "799a3d5751faef254b44",
+    "url": "/static/js/2.a7f9c466.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a7f9c466.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d27ac0a5879ad63c0d64",
+    "url": "/static/js/main.12e54c59.chunk.js"
+  },
+  {
+    "revision": "a0b77f8f506250700787",
+    "url": "/static/js/runtime-main.b9dd4299.js"
+  }
+]);
